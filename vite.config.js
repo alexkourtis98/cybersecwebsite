182996bd15bbbe518cb4d18a89e1b7a87aea6d22
Alexandros Kourtis <Alexandros.Kourtis@acg.edu>
2023-06-21 15:13:20 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
             reactivityTransform: true,
         }),
     ],
-    base: '/dist/',
+    base: '/cybersecwebsite/dist/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
